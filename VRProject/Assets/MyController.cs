6ec9ce4eef8628bc39ce1a2122e8442f3bdd0c63
CoolComp <marc@sevilayha.com>
2018-11-17 15:39:03 -0800

    Player and Initial Player Movement

---
+++ VRProject/Assets/MyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyController : MonoBehaviour {
+
+    public int speed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //get input
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //update player position on input
+        Vector3 position = transform.position;      // this transform
+        position.x += moveHorizontal * speed * Time.deltaTime;
+        position.z += moveVertical * speed * Time.deltaTime;
+        transform.position = position;
+	}
+}
