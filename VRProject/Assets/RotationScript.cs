75b90839c498be371c19a788de8338c200ede031
CoolComp <marc@sevilayha.com>
2018-11-19 00:15:58 -0800

    Boundaries follow camera
    The level rotates and moves with the camera, so it always stays in view

--- VRProject/Assets/RotationScript.cs
+++ VRProject/Assets/RotationScript.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class RotationScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float tiltSpeed = 0.0f;
+    public float tiltAngle = 120.0f;
+    public int rotationAmount = 20;
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float tiltOnY = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltOnX = Input.GetAxis("Vertical") * tiltAngle;
+
+        Quaternion rotation = Quaternion.Euler(tiltOnX, tiltOnY, 0);
+        gameObject.transform.parent = Camera.main.transform;
+        Transform cameraRotation = transform.parent;
+
+        //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * tiltSpeed);
+        
+        
 	}
 }
